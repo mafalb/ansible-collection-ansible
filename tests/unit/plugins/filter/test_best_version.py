7598ae1ac37cfb58fb5f18963810a16ba6265c0d
Markus Falb <markus.falb@mafalb.at>
2021-11-07 01:34:09 +0100

    make lint happy

--- tests/unit/plugins/filter/test_best_version.py
+++ tests/unit/plugins/filter/test_best_version.py
@@ -19,7 +19,7 @@ TEST_CASES = (
     (['_ansible==2.11.5'], '2.11.5'),
     (['_ansible<2.11.6'], '2.11.5'),
     (['_ansible~=2.9.6'], '2.9.27'),
-    (['ansible','_ansible==2.9.10','_ansible_test'], '2.9.10'),
+    (['ansible', '_ansible==2.9.10', '_ansible_test'], '2.9.10'),
     (['_ansible'], '2.11.6'),
     (['_ansible~=2.11.6', '_ansible_test'], '2.11.6'),
 )
