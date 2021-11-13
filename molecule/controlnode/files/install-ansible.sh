3f68417235a82565d1ad87beac298dddb58f9525
Markus Falb <markus.falb@mafalb.at>
2021-11-13 19:14:41 +0100

    fix shellcheck

--- molecule/controlnode/files/install-ansible.sh
+++ molecule/controlnode/files/install-ansible.sh
@@ -1,7 +1,9 @@
-ANSIBLE_COLLECTIONS_PATHS=/root ansible-playbook -D /dev/stdin << PLAYBOOK
 # Copyright (c) 2019-2021 Markus Falb <markus.falb@mafalb.at>
 # GNU General Public License v3.0+
 # see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt
+
+
+ANSIBLE_COLLECTIONS_PATHS=/root ansible-playbook -D /dev/stdin << PLAYBOOK
 ---
 
 - hosts: localhost
