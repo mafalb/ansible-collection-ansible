4ea38a672d20e870f135b8c550aeba42aff4c58b
Markus Falb <markus.falb@mafalb.at>
2024-01-05 14:20:53 +0100

    make lint happy

--- plugins/modules/ansible_info.py
+++ plugins/modules/ansible_info.py
@@ -2,7 +2,9 @@
 # Copyright (c) 2020 RedHat, 2021 Markus Falb
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import (absolute_import, division, print_function)
+# flake8: noqa: E501
+
+from __future__ import ( absolute_import, division, print_function)
 
 __metaclass__ = type
 
