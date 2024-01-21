6af4a92a5ee0838c5597184eea4f53f680d325b2
Markus Falb <markus.falb@mafalb.at>
2024-01-21 01:16:57 +0100

    use constraints file

--- plugins/action/virtualenv.py
+++ plugins/action/virtualenv.py
@@ -201,8 +201,9 @@ class ActionModule(ActionBase):
         if 'src' in module_args:
             del module_args['src']
         if extra_args is not None:
-            module_args['extra_args'] = extra_args
+            module_args['extra_args'] += " " + extra_args
         display.v(module_args['virtualenv_command'])
+        display.v(module_args.get('extra_args', None))
         ret = self._execute_module(module_name='pip', module_args=module_args, task_vars=task_vars)
         return ret
 
@@ -250,6 +251,7 @@ class ActionModule(ActionBase):
             ret2['item'] = "Template constraints file"
             if ret2.get('failed'):
                 raise AnsibleActionFail("Template constraints file has failed: {e}".format(e=ret2))
+            self._task.args['extra_args'] = self._task.args.get('extra_args', '') + "-c " + self._task.args.get('virtualenv') + "/constraints.txt"
 
         # Install workarounds for old ansible versions
         #
